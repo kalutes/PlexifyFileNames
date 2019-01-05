f4d9b547e0083758ccea5f03adde2565786e5e83
Kameron Lutes <kalutes@purdue.edu>
2019-01-05 02:25:36 -0500

    Fix error in begin parameter and subtitle extension parameter

--- PlexifyFileNames.ps1
+++ PlexifyFileNames.ps1
@@ -1,16 +1,17 @@
 param([string]$path = "",
 		[string]$showName = "",
 		[string]$seasonNumber = "",
-		[string]$subtitleExt = ""
+		[string]$subtitleExt = "",
+		[int32]$begin = 1
 )
 
 $files = Get-ChildItem -File $path | Sort-Object
 
 for ($i = 0; $i -lt $files.Count; $i++) {
-	$episode = $i + 1
+	$episode = $i + $begin
 	$fullName = $files[$i].FullName
 	$extension = $files[$i].Extension
-	$destination = "$($path)\$($showName) - s$("{0:d2}" -f [int32]$seasonNumber)e$("{0:d2}" -f [int32]$episode).$($subtitleExt)$($extension)"
+	$destination = "$($path)\$($showName) - s$("{0:d2}" -f [int32]$seasonNumber)e$("{0:d2}" -f [int32]$episode)$($subtitleExt)$($extension)"
 	Write-Output "Moving $($fullName) to $($destination)"
     Move-Item -LiteralPath "$fullName" -Destination "$destination"
 }
