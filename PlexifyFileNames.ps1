55e6e3b72482070a43e90b573e3c35cbb39aeda2
Kameron Lutes <kalutes@purdue.edu>
2019-01-05 01:51:21 -0500

    Change to use literalpath in move operation

--- PlexifyFileNames.ps1
+++ PlexifyFileNames.ps1
@@ -9,7 +9,9 @@ for ($i = 0; $i -lt $files.Count; $i++) {
 	$episode = $i + 1
 	$fullName = $files[$i].FullName
 	$extension = $files[$i].Extension
-    Move-Item -Path $fullName -Destination "$($path)\$($showName) - s$("{0:d2}" -f [int32]$seasonNumber)e$("{0:d2}" -f [int32]$episode)$($extension)"
+	$destination = "$($path)\$($showName) - s$("{0:d2}" -f [int32]$seasonNumber)e$("{0:d2}" -f [int32]$episode)$($extension)"
+	Write-Output "Moving $($fullName) to $($destination)"
+    Move-Item -LiteralPath "$fullName" -Destination "$destination"
 }
 
 exit
